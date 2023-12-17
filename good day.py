3585d0d5c7fab2c20f25a6f116325690a7c7dec9
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2023-12-17 17:31:34 +0200

    Add files via upload

---
+++ good day.py
@@ -0,0 +1,9 @@
+fun = int(input("who use super pets in pet simulator 99?"))
+good = int(input("who played in gta samp ar?"))
+top = int(input("minecraft nice game?"))
+year = int(input("who use cars in ar?"))
+interesting = int(input("ar hard rp game?"))
+
+fort = fun + good + top + year + interesting
+hole = fort / 5
+print(hole)
---
+++ main.py
@@ -0,0 +1,4 @@
+xd = int(input("who use super pets in pet simulator 99 ?"))
+good = xd /1000
+fun = xd *1000
+print(fun,good)
